3f8076a61e61cc2d0356528e7d690c12e58bcda3
Bari LeBari <blebari@mit.edu>
2025-06-25 18:23:59 +0000

    working graph with accurate names of the edges

--- src/interstate.ml
+++ src/interstate.ml
@@ -1,12 +1,18 @@
 open! Core
 module City = String
 
+module Interstate = struct
+  include String
+
+  let default = ""
+end
+
 module Highway_System = struct
   (* We can represent our social network graph as a set of connections, where a connection
      represents a friendship between two people. *)
   module Connection = struct
     module T = struct
-      type t = City.t * City.t [@@deriving compare, sexp]
+      type t = City.t * City.t * Interstate.t [@@deriving compare, sexp]
     end
 
     (* This funky syntax is necessary to implement sets of [Connection.t]s. This is needed
@@ -16,16 +22,25 @@ module Highway_System = struct
 
     let of_string s =
       (* let open! Core in *)
-      let new_s = String.substr_replace_all s ~pattern:"." ~with_:"" in
-      let new_s = String.substr_replace_all new_s ~pattern:" " ~with_:"_" in
+
+      (* string is formated because the api does not work well with . *)
+      let format_string =
+        String.substr_replace_all s ~pattern:"." ~with_:""
+      in
+      let new_s =
+        String.substr_replace_all format_string ~pattern:" " ~with_:"_"
+      in
+      let highway =
+        Option.value_exn (List.hd (String.split new_s ~on:','))
+      in
       let cities_in_interstate = List.tl (String.split new_s ~on:',') in
       let reversed_cities =
         List.rev (Option.value_exn cities_in_interstate)
       in
       let make_pair city1 city2 =
-        City.of_string city1, City.of_string city2
+        City.of_string city1, City.of_string city2, highway
       in
-      let rec get_connections cities : (string * string) list =
+      let rec get_connections cities : (string * string * string) list =
         match List.length cities with
         | 0 -> []
         | _ ->
@@ -44,6 +59,10 @@ module Highway_System = struct
   end
 
   type t = Connection.Set.t [@@deriving sexp_of]
+
+  (* let get_interstate_from_string s =
+     Option.value_exn (List.hd (String.split s ~on:','))
+     ;; *)
 
   let of_file input_file =
     let connections =
@@ -76,7 +95,7 @@ let load_command =
 (* ignore (input_file : File_path.t);
         failwith "TODO"] *)
 
-module G = Graph.Imperative.Graph.Concrete (City)
+module G = Graph.Imperative.Graph.ConcreteLabeled (City) (Interstate)
 
 module Dot = Graph.Graphviz.Dot (struct
     include G
@@ -85,7 +104,7 @@ module Dot = Graph.Graphviz.Dot (struct
        graph. Check out the ocamlgraph graphviz API
        (https://github.com/backtracking/ocamlgraph/blob/master/src/graphviz.mli) for
        examples of what values can be set here. *)
-    let edge_attributes _ = [ `Dir `None ]
+    let edge_attributes e = [ `Dir `None; `Label (snd3 e) ]
     let default_edge_attributes _ = []
     let get_subgraph _ = None
     let vertex_attributes v = [ `Shape `Box; `Label v; `Fillcolor 1000 ]
@@ -117,12 +136,10 @@ let visualize_command =
       fun () ->
         let network = Highway_System.of_file input_file in
         let graph = G.create () in
-        Set.iter network ~f:(fun (person1, person2) ->
+        Set.iter network ~f:(fun (person1, person2, interstate) ->
           (* [G.add_edge] auomatically adds the endpoints as vertices in the graph if
              they don't already exist. *)
-          print_string ("\nCity1: " ^ person1);
-          print_string ("\nCity2: " ^ person2);
-          G.add_edge graph person1 person2);
+          G.add_edge_e graph (person1, interstate, person2));
         Dot.output_graph
           (Out_channel.create (File_path.to_string output_file))
           graph;
